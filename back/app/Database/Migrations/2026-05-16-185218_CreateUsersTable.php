bdcead1803f8762fc84e61f0193b26fb60ce22ac
Pedrog1999 <pedrogianibelli1999@gmail.com>
2026-05-16 16:08:43 -0300

    .env funcional

---
+++ back/app/Database/Migrations/2026-05-16-185218_CreateUsersTable.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'password_hash' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'role' => [
+                'type'       => 'ENUM',
+                'constraint' => ['user', 'admin'],
+                'default'    => 'user',
+            ],
+            'avatar_url' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => true,
+            ],
+            'bio' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'is_banned' => [
+                'type'    => 'TINYINT',
+                'constraint' => 1,
+                'default' => 0,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true); // PRIMARY KEY
+        $this->forge->addUniqueKey('username');
+        $this->forge->addUniqueKey('email');
+
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
